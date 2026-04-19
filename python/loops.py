011dd5c0ac1c558944b9526d8a27eb9f820fd652
Mohd-kaif123 <mansoorikaif365@gmail.com>
2026-04-19 12:02:02 +0000

    commit python concept

---
+++ python/loops.py
@@ -0,0 +1,25 @@
+# for loop
+'''for i in range(1,5):
+    print(f"checking Server {i}....")
+
+#while loop
+server_up= False
+while not server_up:
+    print("waiting for the server to start...")
+    server_up = True    # condition to break the loop
+print("Servver is a up and running!")
+
+#while loop
+i = 1
+while i<=10:
+    print(f"Checking server {i}...")
+    i+=1'''
+
+#While loop
+j=10
+
+while j<=20:
+    if j == 25:
+        print("Server 25 is up and running!")
+        break
+    j+=1
